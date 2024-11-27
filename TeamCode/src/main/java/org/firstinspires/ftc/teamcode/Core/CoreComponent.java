34a5bbc5af3e443fbb486ca81b0b598723c95895
CzrCraft <czrcraft@gmail.com>
2024-11-27 22:07:44 +0200

    started work on core

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/CoreComponent.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Core;
+
+import java.util.ArrayList;
+
+public class CoreComponent {
+    public boolean active = false;
+    public ArrayList<String> info = new ArrayList<>();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/InterfaceType.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.Core;
+
+public enum InterfaceType {
+    INPUT,
+    OUTPUT
+}
